3ac63f71690a8bc6a5cd3875d1b5c2fb57eceec1
bej-syan <all_bright@live.com>
2017-07-03 23:39:28 +0800

    finish the exercise

--- src/main/scala/com/syan/essentialscala/generics/variance/Calculator.scala
+++ src/main/scala/com/syan/essentialscala/generics/variance/Calculator.scala
@@ -1,27 +1,61 @@
 package com.syan.essentialscala.generics.variance
 
-sealed trait Sum[???, ???] {
-  def fold
+sealed trait Sum[+A, +B] {
+  def fold[C](error: A => C, success: B => C): C =
+    this match {
+      case Failure(v) => error(v)
+      case Success(v) => success(v)
+    }
 
-  def map
+  def map[C](f: B => C): Sum[A, C] =
+    this match {
+      case Failure(v) => Failure(v)
+      case Success(v) => Success(f(v))
+    }
 
-  def flatMap
+  def flatMap[AA >: A, C](f: B => Sum[AA, C]): Sum[AA, C] =
+    this match {
+      case Failure(v) => Failure(v)
+      case Success(v) => f(v)
+    }
 }
 
-final case class Failure[???](value: ???) extends Sum[???, ???]
-final case class Success[???](value: ???) extends Sum[???, ???]
+final case class Failure[A](value: A) extends Sum[A, Nothing]
+
+final case class Success[B](value: B) extends Sum[Nothing, B]
 
 
 sealed trait Expression {
-  def eval: Sum[] = ???
+  def eval: Sum[String, Double] =
+    this match {
+      case Addition(l, r) => lift2(l, r, (left, right) => Success(left + right))
+      case Subtraction(l, r) => lift2(l, r, (left, right) => Success(left - right))
+      case Division(l, r) => lift2(l, r, (left, right) =>
+        if (right == 0) Failure("Division by zero") else Success(left / right))
+      case SquareRoot(v) =>
+        v.eval flatMap { value =>
+          if(value < 0) Failure("Square root of negative number")
+          else Success(Math.sqrt(value))
+        }
+      case Number(v) => Success(v)
+    }
 
-  def lift2(l: Expression, r: Expression, f: (Double, Double) => Sum[String, Double]) = ???
+  def lift2(l: Expression, r: Expression, f: (Double, Double) => Sum[String, Double]) =
+    l.eval flatMap { left =>
+      r.eval flatMap { right =>
+        f(left, right)
+      }
+    }
 }
 
 final case class Addition(left: Expression, right: Expression) extends Expression
+
 final case class Subtraction(left: Expression, right: Expression) extends Expression
+
 final case class Division(left: Expression, right: Expression) extends Expression
+
 final case class SquareRoot(value: Expression) extends Expression
+
 final case class Number(value: Double) extends Expression
 
 
